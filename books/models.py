d4d869aa481aad91f7106ccc6800930de97cd829
olexandra.kupriichuk <oleksandra.kupriichuk@skelar.tech>
2026-08-14 12:45:24 +0300

    feat: add books CRUD with admin permissions

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HARD", "Hardcover"
+        SOFT = "SOFT", "Softcover"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=Cover)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
